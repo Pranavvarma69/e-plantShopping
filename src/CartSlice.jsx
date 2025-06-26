2a459757cc46133dce915fd8ec8b0ef5de20e869
Pranavvarma69 <pranavrudraraju6@gmail.com>
2025-06-26 02:43:55 -0400

    Initial commit: Add Paradise Nursery product list and cart

--- src/CartSlice.jsx
+++ src/CartSlice.jsx
@@ -7,11 +7,26 @@ export const CartSlice = createSlice({
   },
   reducers: {
     addItem: (state, action) => {
+        const newitem=action.payload
+        const existingitem=state.items.find(item=>item.name===newitem.name)
+        if(existingitem){
+            existingitem.quantity+=1
+        }
+        else{
+            state.items.push({...newitem,quantity:1})
+        }
     
     },
     removeItem: (state, action) => {
+        const nametoremove=action.payload;
+        state.items.filter(item=>item.name!==nametoremove)
     },
     updateQuantity: (state, action) => {
+        const{name,quantity}=action.payload
+        const itemtoupdate=state.items.find(item=>item.name===name)
+        if(itemtoupdate){
+            itemtoupdate.quantity=quantity
+        }
 
     
     },
